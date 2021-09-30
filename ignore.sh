bb9dfd1766adc2dfa7001e4f2d4216eb48c24569
Chris Hannaby <chris.hannaby@netlify.com>
2021-09-30 13:53:27 -0600

    update ignore

--- ignore.sh
+++ ignore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -s -o /dev/null -w "Response Status for https://api.netlify.com/api/v1/deploys/$DEPLOY_ID/cancel : %{http_code}" -H "Authorization: Bearer $NETLIFY_CANCEL_TOKEN" -d {}  https://api.netlify.com/api/v1/deploys/$DEPLOY_ID/cancel
+# curl -s -o /dev/null -w "Response Status for https://api.netlify.com/api/v1/deploys/$DEPLOY_ID/cancel : %{http_code}" -H "Authorization: Bearer $NETLIFY_CANCEL_TOKEN" -d {}  https://api.netlify.com/api/v1/deploys/$DEPLOY_ID/cancel
 exit 0
