3baea15ff1f899e10716f41f3a3db6457f6c21bc
Chris Hannaby <chris.hannaby@netlify.com>
2022-08-31 12:03:35 -0600

    use henry's bot protection example

--- netlify/edge-functions/ip-block.js
+++ netlify/edge-functions/ip-block.js
@@ -1,17 +1,42 @@
-const ALLOWED_IPS = ["98.38.232.100"];
-
 export default async (request, context) => {
-  const clientIp = context.ip;
-  if (!ALLOWED_IPS.includes(clientIp)) {
-    return new Response(
-      `We're sorry, your IP, ${clientIp}, is not part of the allow list for this page`,
-      {
-        headers: { "content-type": "text/html" },
-        status: 451,
-      }
-    );
-  }
+  //Import allow lists from Env variables
+  const geoAllowList = JSON.parse(Deno.env.get("GEO_ALLOW_LIST"));
 
-  const response = await context.next();
-  return response;
+  //Fetch geo code and IP from incoming request
+  const geoCode = context.geo?.country?.code;
+  const ip = context.ip;
+
+  // Uses https://www.ipqualityscore.com/documentation/proxy-detection/overview to check for VPN and BOT traffic.
+  const ipCheck = await (
+    await fetch(
+      `https://ipqualityscore.com/api/json/ip/abvO9dUu9Ej84y2TOG0UpWwHJtYfwnFI/${ip}?strictness=1&allow_public_access_points=true`
+    )
+  ).json();
+
+  //Complete checks against lists and fraud score
+  const correctGeo = geoAllowList.includes(geoCode);
+  const lowFraudScore = ipCheck.fraud_score < 50;
+
+  //Allows/Denies Access and rewrites to respective pages.
+  if (correctGeo && lowFraudScore) {
+    context.log(
+      `Access Granted - IP Address ${ip} is allowed from ${geoCode}. Fraud score = ${ipCheck.fraud_score}`
+    );
+    return context.next();
+  } else {
+    let errors = [];
+    !correctGeo
+      ? errors.push(` Accessing site from blocked location: ${geoCode}`)
+      : "";
+    !lowFraudScore
+      ? errors.push(
+          ` Accessing site from IP with high fraud score (${ipCheck.fraud_score}), suspected VPN`
+        )
+      : "";
+    context.log(`Access Denied -${errors.toString()}`);
+    return new Response(errors.join(", "), {
+      headers: { "content-type": "text/html" },
+      status: 451,
+    });
+  }
 };
