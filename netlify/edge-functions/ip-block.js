b3da8076a71d7b5f43ccba829499806254c30fc1
Chris Hannaby <chris.hannaby@netlify.com>
2023-02-01 10:50:48 -0700

    update edge function

--- netlify/edge-functions/ip-block.js
+++ netlify/edge-functions/ip-block.js
@@ -1,40 +1,16 @@
-export default async (request, context) => {
-  //Import allow lists from Env variables
-  const geoAllowList = JSON.parse(Deno.env.get("GEO_ALLOW_LIST"));
+const ipAllowList = Deno.env.get("IP_ALLOW_LIST") || "98.38.232.100";
 
-  //Fetch geo code and IP from incoming request
-  const geoCode = context.geo?.country?.code;
+export default async (_, context) => {
   const ip = context.ip;
+  const allowedIp = ipAllowList.includes(ip);
 
-  // Uses https://www.ipqualityscore.com/documentation/proxy-detection/overview to check for VPN and BOT traffic.
-  const ipCheck = await (
-    await fetch(
-      `https://ipqualityscore.com/api/json/ip/abvO9dUu9Ej84y2TOG0UpWwHJtYfwnFI/${ip}?strictness=1&allow_public_access_points=true`
-    )
-  ).json();
-
-  //Complete checks against lists and fraud score
-  const correctGeo = geoAllowList.includes(geoCode);
-  const lowFraudScore = ipCheck.fraud_score < 50;
-
-  //Allows/Denies Access and rewrites to respective pages.
-  if (correctGeo && lowFraudScore) {
-    context.log(
-      `Access Granted - IP Address ${ip} is allowed from ${geoCode}. Fraud score = ${ipCheck.fraud_score}`
-    );
+  if (allowedIp) {
+    context.log(`Access Granted - IP Address ${ip} is allowed`);
     return context.next();
   } else {
-    let errors = [];
-    !correctGeo
-      ? errors.push(` Accessing site from blocked location: ${geoCode}`)
-      : "";
-    !lowFraudScore
-      ? errors.push(
-          ` Accessing site from IP with high fraud score (${ipCheck.fraud_score}), suspected VPN`
-        )
-      : "";
-    context.log(`Access Denied -${errors.toString()}`);
-    return new Response(errors.join(", "), {
+    const error = `Access Denied - IP Address ${ip} is not in allow list`;
+    context.log(error);
+    return new Response(errors, {
       headers: { "content-type": "text/html" },
       status: 451,
     });
